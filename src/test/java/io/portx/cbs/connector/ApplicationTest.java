e874975a6aadcb493260322b337bbba4421f5475
Shashikant Hirugade <shashikant.hirugade@modusbox.com>
2022-08-24 19:56:15 -0500

    feat: added test for xml route

--- src/test/java/io/portx/cbs/connector/ApplicationTest.java
+++ src/test/java/io/portx/cbs/connector/ApplicationTest.java
@@ -1,48 +1,45 @@
 package io.portx.cbs.connector;
 
+
 import io.portx.camel.test.APITest;
 import io.portx.camel.test.util.TestResourceReader;
 import org.apache.camel.Exchange;
 import org.apache.camel.ExchangePattern;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes=Application.class)
 public class ApplicationTest extends APITest {
-    private Logger log = LoggerFactory.getLogger(ApplicationTest.class);
 
     @Test
-    public void testMySampleRoute() throws Exception {
-        Exchange exchange = sendTestRequest("direct:mySampleRoute", ExchangePattern.InOut, new Processor() {
+    public void testGetBranches() throws Exception {
+        Exchange exchange = sendTestRequest("direct:findBranches", ExchangePattern.InOut, new Processor() {
             @Override
             public void process(Exchange exchange) throws Exception {
                 Message in = exchange.getIn();
-                in.setHeader("id", "123");
+                System.out.println(in);
             }
         });
 
         String response = exchange.getIn().getBody(String.class);
         assertNotNull(response);
+        JSONAssert.assertEquals(TestResourceReader.readFileAsString("test-data/json/mambuAPI/getBranchesResponse.json"), response, true);
     }
-
     @Override
     public String[] getMockedRouteIDs() {
-        return new String[] {
-                                "mySampleRoute"
+        return new String[] { "findBranchesRoute"
                             };
     }
 
     @Override
     public String[] getMockedURIs() {
-        return new String[] { "https://foo.bar*" };
+        return new String[] { "https://modusbox.sandbox.mambu.com*" };
+        
     }
 
     @Override
